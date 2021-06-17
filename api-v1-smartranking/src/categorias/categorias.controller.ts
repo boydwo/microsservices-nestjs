c576b430b907975286e27b10d5b1660836054d63
Marcos Tulio Pajé <marcostulio06@hotmail.com>
2021-06-17 07:42:46 -0300

    feature Categorias module part 2 - CRUD

--- api-v1-smartranking/src/categorias/categorias.controller.ts
+++ api-v1-smartranking/src/categorias/categorias.controller.ts
@@ -1,6 +1,8 @@
-import { Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Put } from '@nestjs/common';
+import { Get, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { Body, Controller } from '@nestjs/common';
 import { CategoriasService } from './categorias.service';
+import { AtualizarCategoriaDto } from './dtos/atulizar-categoria.dto';
 import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
 import { Categoria } from './interfaces/categoria.interface';
 
@@ -14,5 +16,34 @@ export class CategoriasController {
     @Body() criarCategoriaDto: CriarCategoriaDto,
   ): Promise<Categoria> {
     return await this.categoriasService.criarCategoria(criarCategoriaDto);
+  }
+
+  @Get()
+  async consultarCategorias(): Promise<Array<Categoria>> {
+    return await this.categoriasService.consultarTodasCategorias();
+  }
+
+  @Get('/:categoria')
+  async consultarCategoriaPeloId(
+    @Param('categoria') categoria: string,
+  ): Promise<Categoria> {
+    return await this.categoriasService.consultarCategoriaPeloId(categoria);
+  }
+
+  @Put('/:categoria')
+  @UsePipes(ValidationPipe)
+  async atualizarCategoria(
+    @Body() atualizarCategoriaDto: AtualizarCategoriaDto,
+    @Param('categoria') categoria: string,
+  ): Promise<void> {
+    await this.categoriasService.atualizarCategoria(
+      categoria,
+      atualizarCategoriaDto,
+    );
+  }
+
+  @Post('/:categoria/jogadores/:idJogador')
+  async atribuirCategoriaJogador(@Param() params: string[]): Promise<void> {
+    return await this.categoriasService.atribuirCategoriaJogador(params);
   }
 }
--- api-v1-smartranking/src/categorias/categorias.service.ts
+++ api-v1-smartranking/src/categorias/categorias.service.ts
@@ -1,6 +1,11 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { AtualizarCategoriaDto } from './dtos/atulizar-categoria.dto';
 import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
 import { Categoria } from './interfaces/categoria.interface';
 
@@ -25,5 +30,62 @@ export class CategoriasService {
     const categoriaCriada = new this.categoriaModel(criarCategoriaDto);
 
     return await categoriaCriada.save();
+  }
+
+  async consultarTodasCategorias(): Promise<Array<Categoria>> {
+    return await this.categoriaModel.find().exec();
+  }
+
+  async consultarCategoriaPeloId(categoria: string): Promise<Categoria> {
+    const categoriaEncontrada = await this.categoriaModel
+      .findOne({
+        categoria,
+      })
+      .exec();
+
+    if (!categoriaEncontrada) {
+      throw new NotFoundException(`Categoria ${categoria} não encontrada!`);
+    }
+
+    return categoriaEncontrada;
+  }
+
+  async atualizarCategoria(
+    categoria: string,
+    atualizarCategoriaDto: AtualizarCategoriaDto,
+  ): Promise<void> {
+    const { descricao, eventos } = atualizarCategoriaDto;
+
+    const categoriaEncontrada = await this.categoriaModel
+      .findOne({
+        categoria,
+      })
+      .exec();
+
+    if (!categoriaEncontrada) {
+      throw new NotFoundException(`Categoria ${categoria} não encontrada!`);
+    }
+
+    categoriaEncontrada.descricao = descricao;
+    categoriaEncontrada.eventos = eventos;
+
+    await categoriaEncontrada.save();
+  }
+
+  async atribuirCategoriaJogador(params: string[]): Promise<void> {
+    const categoria = params['categoria'];
+    const idJogador = params['idJogador'];
+
+    const categoriaEncontrada = await (
+      await this.categoriaModel.findOne({ categoria })
+    ).execPopulate();
+
+    if (!categoriaEncontrada) {
+      throw new BadRequestException(`Categoria ${categoria} não encontrada!`);
+    }
+
+    categoriaEncontrada.jogadores.push(idJogador);
+
+    await categoriaEncontrada.save();
   }
 }
