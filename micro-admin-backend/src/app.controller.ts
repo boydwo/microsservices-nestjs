93e80b98bf162ae4cfd706b4904a221c75051baf
Marcos Tulio Pajé <marcostulio06@hotmail.com>
2021-07-10 16:06:13 -0300

    feature: ack messages (Received messages confirmation)

--- micro-admin-backend/src/app.controller.ts
+++ micro-admin-backend/src/app.controller.ts
@@ -1,18 +1,42 @@
 import { Controller, Get, Logger } from '@nestjs/common';
-import { EventPattern, MessagePattern, Payload } from '@nestjs/microservices';
+import {
+  Ctx,
+  EventPattern,
+  MessagePattern,
+  Payload,
+  RmqContext,
+} from '@nestjs/microservices';
 import { AppService } from './app.service';
 import { Categoria } from './interfaces/categorias/categoria.interface';
 
+const ackErrors: string[] = ['E11000'];
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
   private logger = new Logger(AppController.name);
 
   @EventPattern('criar-categoria')
-  async criarCategoria(@Payload() categoria: Categoria) {
+  async criarCategoria(
+    @Payload() categoria: Categoria,
+    @Ctx() context: RmqContext,
+  ) {
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+
     this.logger.log(`categoria: ${JSON.stringify(categoria)}`);
 
-    await this.appService.criarCategoria(categoria);
+    try {
+      await this.appService.criarCategoria(categoria);
+      await channel.ack(originalMsg); // envia corfimação de recebimento e pode ser retirada da fila.
+    } catch (error) {
+      this.logger.error(`error: ${JSON.stringify(error.message)}`);
+
+      ackErrors.map(async (ackError) => {
+        if (error.message.includes(ackError)) {
+          await channel.ack(error.message);
+        }
+      });
+    }
   }
   @MessagePattern('consultar-categorias')
   async consultarCategorias(@Payload() _id: string) {
