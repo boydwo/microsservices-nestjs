dd821bdf42e73ffec89b0cc1678810561e5532a7
Marcos Tulio Pajé <marcostulio06@hotmail.com>
2021-07-19 06:58:57 -0300

    fix: refactor micro-admin

--- micro-admin-backend/src/jogadores/jogadores.service.ts
+++ micro-admin-backend/src/jogadores/jogadores.service.ts
@@ -24,7 +24,10 @@ export class JogadoresService {
 
   async consultarTodosJogadores(): Promise<Jogador[]> {
     try {
-      return await this.jogadorModel.find().populate('categoria').exec();
+      return await this.jogadorModel
+        .find()
+        // .populate('categoria')
+        .exec();
     } catch (error) {
       this.logger.error(`error: ${JSON.stringify(error.message)}`);
       throw new RpcException(error.message);
@@ -35,7 +38,7 @@ export class JogadoresService {
     try {
       return await this.jogadorModel
         .findOne({ _id })
-        .populate('categoria')
+        // .populate('categoria')
         .exec();
     } catch (error) {
       this.logger.error(`error: ${JSON.stringify(error.message)}`);
